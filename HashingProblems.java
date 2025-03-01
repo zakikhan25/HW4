869c38aba3708e26eff2ac72942308e263748f8b
zakikhan25 <zkhan25@luc.edu>
2025-02-28 22:00:48 -0600

    Update HashingProblems.java

--- HashingProblems.java
+++ HashingProblems.java
@@ -1,8 +1,8 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Zaki Khan / 272 001 ***
  *
  * This HashingProblems object contains three methods / problems that you must
- * complete utilize the HashMap object within the Java's Collection Framework Library.
+ * complete utilizing the HashMap object within the Java's Collection Framework Library.
  *
  * The three methods / problems you need to solve are:
  *  - getAverage
@@ -13,7 +13,6 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
 
 class HashingProblems {
 
@@ -32,18 +31,18 @@ class HashingProblems {
      */
 
     public double getAverage(HashMap<Integer, Integer> map, int[] array) {
+        double sum = 0;
+        int count = 0;
 
-        /*
-         * ADD YOUR CODE HERE - DO NOT FORGET TO ADD YOU NAME AT TOP OF FILE
-         *
-         * Note: if NO values found in common between the HashMap and supplied array,
-         * returning 0.0 is NOT correct, as that is not the average value. Whereas
-         * returning 0.0/0.0 IS correct (which would return a non-number).
-         */
+        for (int key : array) {
+            if (map.containsKey(key)) {
+                sum += map.get(key);
+                count++;
+            }
+        }
 
-         return 0.0 / 0.0;
-  }
-
+        return count == 0 ? Double.NaN : sum / count;
+    }
 
     /*
      * Method odd()
@@ -52,65 +51,70 @@ class HashingProblems {
      * values of the corresponding keys that are odd.
      */
 
-  public ArrayList<String> odd(HashMap<Integer, String> map) {
-    
-      ArrayList<String> result = new ArrayList<>();
+    public ArrayList<String> odd(HashMap<Integer, String> map) {
+        ArrayList<String> result = new ArrayList<>();
 
-      /*
-       * ADD YOUR CODE HERE
-       *
-       * Hint: Consider iterating over the HashMap using the keySet method.
-       */
+        for (Integer key : map.keySet()) {
+            if (key % 2 != 0) {
+                result.add(map.get(key));
+            }
+        }
 
+        return result;
+    }
 
-      return result;
-  }
+    /*
+     * Method twoSums()
+     *
+     * You ARE to solve this problem in time complexity O(n). The submittals will be spot checked.
+     *
+     * Problem statement:
+     * Suppose you are given an integer array containing the values [1,4,5,7,8,9] along with the
+     * value k=4, where k is the difference between two array elements. How many times does k appear
+     * in that list?
+     *
+     * With the above numbers, it will be three times:
+     *    k = 4
+     *    (5 - 1) = k
+     *    (8 - 4) = k
+     *    (9 - 5) = k
+     *    k appears 3 times.
+     *
+     * All combinations must be considered. But, any other combination of the numbers in the array
+     * results in a difference value that is not equal to k (k=4 in this case).
+     *
+     * This can be solved using nested for-loops, checking all combinations of the values in the array.
+     * But the time complexity would be O(n^2).
+     *
+     * In order to solve this problem in O(n) complexity, utilize a HashMap (or a HashSet).
+     *
+     * You are to solve this using a HashMap (or you can use a HashSet, which is implemented
+     * using HashMap). To solve this, you should populate the HashMap (or HashSet) based on
+     * the array (this will be complexity time on the order of 'n'). After populating the HashMap,
+     * consider a for-loop that does a lookup (probe) of the HashMap (or HashSet) on each iteration
+     * of the loop. This will also have a complexity on the order of 'n', as the hashing probes are a
+     * constant time complexity (after removing any constant based on collisions).
+     *
+     * This will result in a time complexity of O(n) for the overall method.
+     *
+     * NOTE: Solving using a HashMap or HashSet is fine (either is okay). HashSet may be easier to code?
+     */
 
+    public int twoSums(int[] numbers, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
 
-  /*
-   * Method twoSums()
-   *
-   * You ARE to solve this problem in time complexity O(n). The submittals will be spot checked.
-   *
-   * Problem statement:
-   * Suppose you are given an integer array containing the values [1,4,5,7,8,9] along with the
-   * value k=4, where k is the difference between two array elements. How many times does k appear
-   * in that list?
-   *
-   * With the above numbers, it will be three times:
-   *    k = 4
-   *    (5 - 1) = k
-   *    (8 - 4) = k
-   *    (9 - 5) = k
-   *    k appears 3 times.
-   *
-   * All combinations must be considered. But, any other combination of the numbers in the array
-   * results in a difference value that is not equal to k (k=4 in this case).
-   *
-   * This can be solved using nested for-loops, checking all combinations of the values in the array.
-   * But the time complexity would be O(n^2).
-   *
-   * In order to solve this problem in O(n) complexity, utilize a HashMap (or a HashSet).
-   *
-   * You are two solve this using a HashMap (or you can use a HashSet, which is implemented
-   * using HashMap). To solve this, you should populate the HashMap (or HashSet) based on
-   * the array (this will be complexity time on the order of 'n'). After populating the HashMap,
-   * consider a for-loop that does a lookup (probe) of the HashMap (or HashSet) on each iteration
-   * of the loop. This will also have a complexity on the order of 'n', as the hashing probes are a
-   * constant time complexity (after removing any constant based on collisions).
-   *
-   * This will result in a time complexity of O(n) for the overall method.
-   *
-   * NOTE: Solving using a HashMap or HashSet is fine (either is okay). HashSet may be easier to code?
-   */
+        for (int num : numbers) {
+            if (set.contains(num - k)) {
+                count++;
+            }
+            if (set.contains(num + k)) {
+                count++;
+            }
+            set.add(num);
+        }
 
-  public int twoSums(int[] numbers, int k) {
-
-      /*
-       * ADD YOUR CODE HERE
-       */
-
-      return -1;
-  }
+        return count;
+    }
 
 } /* end class HashingProblems */
